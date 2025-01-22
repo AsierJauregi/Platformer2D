9eba3e64035a63bd092d8479a1b539052f61ae94
Asier Jauregui Rivera <72439067+AsierJauregi@users.noreply.github.com>
2025-01-22 10:43:22 +0100

    Damage feedback added to entities

--- Assets/Script/LifeSystem.cs
+++ Assets/Script/LifeSystem.cs
@@ -6,10 +6,18 @@ public class LifeSystem : MonoBehaviour
 {
     [SerializeField] private string playerTag = "PlayerHitbox";
     [SerializeField] private float lives;
+    [SerializeField] private float damageFeedbackTime = 0.2f;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     public void ReceiveDamage(float damageReceived)
     {
         lives -= damageReceived;
+        StartCoroutine(AnimateHit());
         if(lives <= 0)
         {
             if (this.gameObject.CompareTag(playerTag))
@@ -21,5 +29,13 @@ public class LifeSystem : MonoBehaviour
                 Destroy(this.gameObject);
             }
         }
+    }
+
+    IEnumerator AnimateHit()
+    {
+        spriteRenderer.color = Color.red;
+        yield return new WaitForSeconds(damageFeedbackTime);
+        spriteRenderer.color = Color.white;
+
     }
 }
